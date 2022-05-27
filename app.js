a35d252edc27223e6be58365ed39943de54e7f9e
user2695 <53275443+user2695@users.noreply.github.com>
2022-05-27 17:45:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ swaggerDocument = require('./swagger.json');
 
 
 app.get("/", function (req, res) {
-  const key = "AIzaSyBC_mjYO7w2RI1YQdswliY4KA-ZQvuBtZY";
+  const key = "key here";
   const query = req.body.book;
   const url =
     "https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=" + key;
